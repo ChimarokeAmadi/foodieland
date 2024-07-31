e48b64dc559b9651848a05027aa5c4c18a734a53
ChimarokeAmadi <chirokeamadi@gmail.com>
2024-07-31 02:31:01 +0100

    added simple section and moved recipes to a different folder

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import HeroSection from "@/components/Hero/HeroSection";
 import Categories from "@/components/Categories/Categories";
+import Simple from "@/components/Simple/Simple";
 
 export default function Home() {
 	return (
 		<main className=''>
 			<HeroSection />
 			<Categories />
+			<Simple />
 		</main>
 	);
 }
---
+++ src/components/recipes/recipes.tsx
@@ -0,0 +1,58 @@
+const recipes = [
+	{
+		image: "/Simple/cheeseburger.png",
+		name: "Big and Juicy Wagyu Beef Cheeseburger",
+		time: "30 Minutes",
+		type: "Snack",
+	},
+	{
+		image: "/Simple/salmon.png",
+		name: "Fresh Lime Roasted Salmon with Ginger Sauce",
+		time: "30 Minutes",
+		type: "Fish",
+	},
+	{
+		image: "/Simple/pancakes.png",
+		name: "Strawberry Oatmeal Pancake with Honey Syrup",
+		time: "30 Minutes",
+		type: "Breakfast",
+	},
+	{
+		image: "/Simple/salad.png",
+		name: "Fresh and Healthy Mixed Mayonnaise Salad",
+		time: "30 Minutes",
+		type: "Healthy",
+	},
+	{
+		image: "/Simple/meetballs.png",
+		name: "Chicken Meatballs with Cream Cheese",
+		time: "30 Minutes",
+		type: "Meat",
+	},
+	{
+		image: "",
+		name: "",
+		time: "30 Minutes",
+		type: "",
+	},
+	{
+		image: "/Simple/fruitypancake.png",
+		name: "Fruity Pancake with Orange & Blueberry",
+		time: "30 Minutes",
+		type: "Sweet",
+	},
+	{
+		image: "/Simple/rice.png",
+		name: "The Best Easy One Pot Chicken and Rice",
+		time: "30 Minutes",
+		type: "Snack",
+	},
+	{
+		image: "/Simple/pasta.png",
+		name: "The Creamiest Creamy Chicken and Bacon Pasta",
+		time: "30 Minutes",
+		type: "Noodles",
+	},
+];
+
+export default recipes;

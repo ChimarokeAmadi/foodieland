ca333796beb09fec43cafd29bd7d29d6c0629800
ChimarokeAmadi <chirokeamadi@gmail.com>
2024-08-01 18:33:48 +0100

    fixed some images

--- src/components/Dynamic/Hero/Hero.tsx
+++ src/components/Dynamic/Hero/Hero.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 export default function Hero(props: { image: string; name: string }) {
 	return (
 		<div className='container'>
@@ -6,7 +7,13 @@ export default function Hero(props: { image: string; name: string }) {
 					<p className='text-[64px] font-semibold w-[777px]'>{props.name}</p>
 					<div className='divide-x-[1px] flex '>
 						<div className='flex  gap-4 pr-[58px]'>
-							<img src='/Hero/Ellipse 2.png' alt='' className='size-[50px]' />
+							<Image
+								src='/Hero/Ellipse 2.png'
+								alt=''
+								height={1}
+								width={1}
+								className='size-[50px]'
+							/>
 							<div className='flex flex-col gap-2 justify-center'>
 								<p className='font-bold'>John Smith</p>
 								<p className='font-medium opacity-60 tracking-[-2%] text-[14px]'>
@@ -15,10 +22,15 @@ export default function Hero(props: { image: string; name: string }) {
 							</div>
 						</div>
 						<div className='flex gap-4 px-8 items-center'>
-							<img
+							<Image
 								src='/Dynamic/Hero/Timer.png'
 								alt='timer'
-								className='size-6'
+								height={100}
+								width={100}
+								style={{
+									height: "24px",
+									width: "24px",
+								}}
 							/>
 							<div className='space-y-2'>
 								<p className='text-[12px] font-medium tracking-[2px]'>
@@ -28,10 +40,15 @@ export default function Hero(props: { image: string; name: string }) {
 							</div>
 						</div>
 						<div className='flex gap-4 px-8 items-center'>
-							<img
+							<Image
 								src='/Dynamic/Hero/Timer.png'
 								alt='timer'
-								className='size-6'
+								height={100}
+								width={100}
+								style={{
+									height: "24px",
+									width: "24px",
+								}}
 							/>
 							<div className='space-y-2'>
 								<p className='text-[12px] font-medium tracking-[2px]'>
@@ -41,10 +58,15 @@ export default function Hero(props: { image: string; name: string }) {
 							</div>
 						</div>
 						<div className='pl-8 flex gap-[10px] items-center'>
-							<img
-								src='/Dynamic/Hero/ForkKnife.png'
-								alt=''
-								className='size-6'
+							<Image
+								src='/Hero/ForkKnife.png'
+								alt='timer'
+								height={100}
+								width={100}
+								style={{
+									height: "24px",
+									width: "24px",
+								}}
 							/>
 							<p className='opacity-60 text-[14px]'>Chicken</p>
 						</div>
@@ -53,14 +75,26 @@ export default function Hero(props: { image: string; name: string }) {
 				<div className='flex items-end gap-8 mb-4'>
 					<div className='flex flex-col gap-4 items-center'>
 						<button className='rounded-[100%] p-7 bg-[#E7FAFE] h-fit '>
-							<img src='/Dynamic/Hero/printer.png' alt='' className='size-6' />
+							<Image
+								src='/Dynamic/Hero/printer.png'
+								height={1}
+								width={1}
+								alt=''
+								className='size-6'
+							/>
 						</button>
 
 						<p className='tracking-[2px] text-[12px]'>PRINT</p>
 					</div>
 					<div className='flex flex-col gap-4 items-center'>
 						<button className='rounded-[100%] p-7 bg-[#E7FAFE] h-fit '>
-							<img src='/Dynamic/Hero/share.png' alt='' className='size-6' />
+							<Image
+								src='/Dynamic/Hero/share.png'
+								alt=''
+								height={1}
+								width={1}
+								className='size-6'
+							/>
 						</button>
 
 						<p className='tracking-[2px] text-[12px]'>SHARE</p>
@@ -69,10 +103,20 @@ export default function Hero(props: { image: string; name: string }) {
 			</div>
 			<div className='flex justify-between'>
 				<div className='relative h-fit w-fit'>
-					<img src={props.image} alt='' className='rounded-[30px] w-[840px]' />
-					<img
+					<Image
+						src={props.image}
+						alt='image'
+						style={{ borderRadius: "30px", width: "840px", height: "600px" }}
+						width={840}
+						height={600}
+						className='rounded-[30px] w-[840px]'
+					/>
+					<Image
 						src='/Dynamic/Hero/PlayButton.png'
 						alt=''
+						style={{ width: "120px", height: "120px" }}
+						width={120}
+						height={120}
 						className='size-[120px] absolute top-[220px] left-[360px]'
 					/>
 				</div>
